86199432c741e1c8c91ef1866b344e7be26b4184
Gerardox1029 <60759191@continental.edu.pe>
2025-11-04 09:39:46 -0500

    feat: Versión 2, sin código basura, corrección de colores, falta integrar Función Google Maps

--- DogLi/Anuncios/script_anuncios.js
+++ DogLi/Anuncios/script_anuncios.js
@@ -4,40 +4,6 @@
 document.body.classList.add('page-loaded');
 
 
-
-           if (window.self !== window.top)
-			{
-				window.top.location.href = window.location.href;
-			}
-			/*
-			if (document.referrer.indexOf("gproxx.com") != -1)
-			{
-				window.location = "https://www.perro-perdido.com/es-es/";
-    		}
-			*/
-
-            
-           
-           
-           function analytic_event(params={})
-			{
-				if (window.location)
-				{
-					params['url'] = window.location.href ;
-				}
-
-				if (document.referrer)
-				{
-					params['referrer'] = document.referrer ;
-				}
-
-				$.get("https://www.perro-perdido.com/analytic.php", params);
-			}
-
-
-            
-            
-            
             
             
             window.dataLayer = window.dataLayer || [];
@@ -59,57 +25,8 @@ document.body.classList.add('page-loaded');
 					'data': data
 				});
 			}
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        $(document).ready(function()
-		{
-			$.cookie.json = true;
-			
-			var cookie_user = $.cookie('session');
-			
-			if (cookie_user)
-			{
-				if (cookie_user.user)
-				{
-					$("#user_header").html('<a href="https://www.perro-perdido.com/compte/index.php?language=es&country=es" rel="nofollow">' + cookie_user.user['email'] + '</a>') ;
-					$(".user_auth").hide() ;
-
-					// pour que la notification ne s'affiche pas avant que le compteur de vue soit màj sur la page d'une annonce
-					window.setTimeout(get_notifications,5000);	
-				}
-				else
-				{
-					$("#user_header").html('<a href="https://www.perro-perdido.com/compte/signin.php?language=es&country=es" rel="nofollow"><i class="bi bi-person"></i> Conexíon</a>') ;
-				}
-			}
-			else
-			{
-				$("#user_header").html('<a href="https://www.perro-perdido.com/compte/signin.php?language=es&country=es" rel="nofollow"><i class="bi bi-person"></i> Conexíon</a>') ;
-			}
-			
-		});
-	
-		function get_notifications()
-		{
-			$.get("https://www.perro-perdido.com/user_comments.php", function(data)
-			{
-				if (data > 0)
-				{
-					$("#user_comments_desktop").html('<a href="https://www.perro-perdido.com/compte/comments.php?language=es&country=es" class="bi bi-chat-right-text" style="font-size:1rem;" rel="nofollow"><span class="badge badge-pill badge-danger" style="transform: translate(-10px, -50%)">' + data + '</span></a>');
-					$("#user_comments_mobile").html('<a href="https://www.perro-perdido.com/compte/comments.php?language=es&country=es" class="d-flex align-items-start" rel="nofollow"><i class="bi bi-chat-right-text h4"></i><span class="badge badge-pill badge-danger" style="transform: translate(-10px, -50%)">' + data + '</span></a>');				
-				}
-			});
-		}
-
-
+   
+    
         setTimeout(() => analytic_event({"name":"screen_view"}), 500); 
 
 
